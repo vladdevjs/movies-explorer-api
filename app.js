85a0db730ecdebd1734d64008c8d4f35e3967007
Vladislav Umarov <vladdevs@yandex.ru>
2023-08-05 08:25:40 +0300

    fix: updated domain for cors

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ const { port, mongoURI } = require('./config');
 const app = express();
 
 app.use(helmet());
-app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
+app.use(
+  cors({ origin: 'https://vladmovies.nomoreparties.co', credentials: true })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
